70dbd123fccba4e2ad86327d7f5f7e519b7508a9
Mina Azimi <minaazimi1985@gmail.com>
2018-03-04 18:39:52 +1100

    First Commit

---
+++ js/javascript.js
@@ -0,0 +1,2 @@
+var date = new Date();
+document.querySelector('.date').innerHTML = (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear();
---
+++ js/jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+//  $(".leftColumn").css({"padding-top": "10px"}); //Padding-top for all elements in left column on main content
+  $(".leftColumn img").css({"margin-right":"10px"});
+  $(".leftColumn_desc").css({"cursor": "pointer"});
+  $(".leftColumn_desc .justify").css({"text-align": "justify"});
+  $(".rightColumn").css({"padding-top" : "10px"});
+  $(".rightColumn img").css({"margin-right" : "10px"});
+  $(".rightColumn h1").css({"background-color":"#dee2e6", "font-size":"1.4rem", "padding":"8px" });
+  $(".rightColumn .caption").css({"font-size":"0.76rem"});
+  $("footer border").css({"backgrund-color":"#262f35"});
+});
